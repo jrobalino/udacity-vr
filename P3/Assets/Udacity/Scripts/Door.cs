2791819754f4229bc4e8c50733cb1c0ed61f18ea
J Robalino <J Robalino>
2017-01-07 16:14:08 -0600

    Bake the lighting and finalize scripts.

--- P3/Assets/Udacity/Scripts/Door.cs
+++ P3/Assets/Udacity/Scripts/Door.cs
@@ -10,6 +10,7 @@ public class Door : MonoBehaviour
     public float speed = 0.5f;
     public AudioSource doorOpen;
     public AudioSource doorLocked;
+    public SignPost signPost;
 
     public void Unlock()
     {
@@ -19,6 +20,7 @@ public class Door : MonoBehaviour
 
     public void OnDoorClicked()
     {
+        // When the door is clicked, play appropriate audio and call doorOpen if player has key
         if (keyFound == true)
         {
             doorClicked = true;
@@ -33,17 +35,20 @@ public class Door : MonoBehaviour
 
     public void Update()
     {
+        // If the key has been found and the door is clicked, move it up
         if (keyFound == true && doorClicked == true)
         {
             
             Vector3 endPosition = new Vector3(transform.position.x, 9.0f, transform.position.z);
             transform.position = Vector3.Lerp(transform.position, endPosition, speed * Time.deltaTime);
+            signPost.drawText();
             
         }
     }
 
     public void ResetVariables()
     {
+        // Reset variables when scene restarts
         keyFound = false;
         doorClicked = false;
     }
--- P3/Assets/Udacity/Scripts/SignPost.cs
+++ P3/Assets/Udacity/Scripts/SignPost.cs
@@ -9,18 +9,25 @@ public class SignPost : MonoBehaviour
     public int numCoins = 7;
     static int coins = 0;
     static float apperception = 0;
+    static bool displayText = false;
     Text message;
 
     public void Start()
     {
+        // Set variables to their start values
         coins = 0;
         apperception = 0;
+        displayText = false;
     }
 
     public void Update()
     {
-        message = GetComponent<Text>();
-        message.text = "Coins collected: " + coins + " out of " + numCoins + "\nApperception level: " + apperception + "\n\nPlay again?";
+        // if conditions are met, display the end score
+        if (displayText == true)
+        {
+            message = GetComponent<Text>();
+            message.text = "Coins collected: " + coins + " out of " + numCoins + "\nApperception level: " + apperception + "\n\nPlay again?";
+        }
     }
 
     public void ResetScene() 
@@ -32,9 +39,18 @@ public class SignPost : MonoBehaviour
 
     public void addCoin()
     {
+        // Keep track of coins collected and calculate apperception score
         coins++;
         float coinsFloat = coins * 1.0f;
         float numCoinsFloat = numCoins * 1.0f;
         apperception = Mathf.Round(coinsFloat / numCoinsFloat * 20.0f);
     }
+
+    public void drawText()
+    {
+        // Set this to true when the user opens the door
+        displayText = true;
+    }
+
+
 }
