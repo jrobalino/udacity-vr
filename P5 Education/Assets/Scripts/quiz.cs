a53b37c29ea43276aec0cdbd95dd61992e5edbe6
J Robalino <J Robalino>
2017-02-10 23:53:07 -0600

    Add quiz and ambient sounds.

---
+++ P5 Education/Assets/Scripts/quiz.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class quiz : MonoBehaviour {
+
+    public AudioSource introAudio;
+    public AudioSource wrongAudio;
+    public AudioSource correctAudio;
+    public GameObject wrongAnswers;
+    public GameObject panel;
+    public Button correctButton;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public void wrongAnswer()
+    {
+        introAudio.Stop();
+        wrongAudio.Play();
+
+    }
+
+    public void correctAnswer()
+    {
+        Image successPanel;
+
+        correctButton.enabled = false;
+
+        introAudio.Stop();
+        correctAudio.Play();
+
+        wrongAnswers.SetActive(false);
+        successPanel = panel.GetComponent<Image>();
+        successPanel.color = new Color(.984f, 1f, .839f);
+
+    }
+}
---
+++ P5 Education/Assets/Scripts/score.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class score : MonoBehaviour {
+
+    private int currentScore;
+    public AudioSource winAudio;
+    
+    // Use this for initialization
+	void Start () {
+        currentScore = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public void increaseScore()
+    {
+        currentScore ++;
+        checkScore();
+    }
+
+    public void playWinAudio()
+    {
+        winAudio.Play();
+    }
+
+    public void checkScore()
+    {
+        if (currentScore == 5)
+        {
+            Invoke("playWinAudio", 3f);
+
+        }
+    }
+}
